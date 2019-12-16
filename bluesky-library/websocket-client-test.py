dbafa102637ea7f4b7cb9c437feab52202d776dc
Alex Palma <alexp@ansto.gov.au>
2019-12-16 18:24:26 +1100

    websocket test script: use try statement to handle connection issues

--- bluesky-library/websocket-client-test.py
+++ bluesky-library/websocket-client-test.py
@@ -13,27 +13,48 @@ use_real_helical_scan_plan_make_hardware_move = False
 
 async def test_starting_plan():
     uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
+    try:
+        async with websockets.connect(uri) as websocket:
 
-        plan = "simulated"  # this corresponds with expected label on websocket server in bluesky_plan.py
-        if use_real_helical_scan_plan_make_hardware_move:
-            plan = "helical scan"  # this corresponds with expected label on websocket server in bluesky_plan.py
+            plan = "simulated"  # this corresponds with expected label on
+            # websocket server in bluesky_plan.py
+            if use_real_helical_scan_plan_make_hardware_move:
+                plan = "helical scan"  # this corresponds with expected label
+                # on websocket server in bluesky_plan.py
 
-        # payload = {"type": "start", "plan": plan}
-        payload = {"type": "start", "plan": plan, "params": {"example_param_1": 3, "example_param_2": 4}}
-        await websocket.send(json.dumps(payload))
-        response = await websocket.recv()
-        print(f'I\'m the websocket client and I got this response: {response}')
-        await websocket.close(reason="thats enough testing for now")
+            # payload = {"type": "start", "plan": plan}
+            payload = {
+                "type": "start",
+                "plan": plan,
+                "params": {"example_param_1": 3, "example_param_2": 4}}
+            await websocket.send(json.dumps(payload))
+            response = await websocket.recv()
+            print(f'I\'m the websocket client and I got this ' +
+                  'response: {response}')
+            await websocket.close(reason="thats enough testing for now")
+    except ConnectionResetError:
+        print('The websocket connection was closed by the other end abruptly')
+    except ConnectionError as e:
+        # covers ConnectionRefusedError & ConnectionAbortedError
+        print(f'I was unable to connect to the websocket {uri}')
+        print(str(e))
 
 
 async def test_subscribing_to_updates():
     uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        payload = {"type": "subscribe"}
-        await websocket.send(json.dumps(payload))
-        while True:
-            response = await websocket.recv()
-            print(f'I\'m the websocket subscriber and I got this response: {response}')
+    try:
+        async with websockets.connect(uri) as websocket:
+            payload = {"type": "subscribe"}
+            await websocket.send(json.dumps(payload))
+            while True:
+                response = await websocket.recv()
+                print('I\'m the websocket subscriber and I got this ' +
+                      f'response: {response}')
+    except ConnectionResetError:
+        print('The websocket connection was closed by the other end abruptly')
+    except ConnectionError as e:
+        # covers ConnectionRefusedError & ConnectionAbortedError
+        print(f'I was unable to connect to the websocket {uri}')
+        print(str(e))
 
 asyncio.get_event_loop().run_until_complete(test_starting_plan())
